8fadb70f6a92a6f6a165a5a40c8ecdc82f1c414b
mudigondauma <mudigonda.uma@gmail.com>
2026-04-25 09:49:19 +0000

    supabase config update

---
+++ supabase-config.local.js
@@ -0,0 +1,4 @@
+window.MeridianSupabaseConfig = {
+  url: "https://db.ltlrcnyhakhnbrnihlpm.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0bHJjbnloYWtobmJybmlobHBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwOTIwNTAsImV4cCI6MjA5MjY2ODA1MH0.1YZBXDhwo-KDjSBGWSavEHz5D3YHvBscRbyTXV79Zo4",
+};
